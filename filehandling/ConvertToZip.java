48e23c1f00f5f4eb33f1d8eab247480c64afe600
Subhragoswami <109273635+Subhragoswami@users.noreply.github.com>
2022-08-11 17:36:02 +0530

    Update ConvertToZip.java

--- filehandling/ConvertToZip.java
+++ filehandling/ConvertToZip.java
@@ -9,9 +9,9 @@ import java.util.zip.GZIPOutputStream;
 public class ConvertToZip {
 	public static void main(String[] args) {
 
-		String file = "C:\\Users\\Admin\\eclipse-workspace\\Java\\FileHandlingExmple.txt";
-		String gzipFile = "C:\\\\Users\\\\Admin\\\\eclipse-workspace\\\\Java\\\\FileHandlingExmple.gz";
-		String newFile = "C:\\\\Users\\\\Admin\\\\eclipse-workspace\\\\Java\\\\FileHandlingExmple.txt";
+		String file = "FileHandling\\FileHandlingExmple.txt";
+		String gzipFile = "FileHandling\\FileHandlingExmple.gz";
+		String newFile = "FileHandling\\FileHandlingExmple.txt";
 		// Converting text file to compressed gzip file
 		compressGzipFile(file, gzipFile);
 		// Converting gzip file to normal text file again
@@ -23,21 +23,31 @@ public class ConvertToZip {
 	 * This method is used to decompress the GZIP file to normal text file
 	 */
 	private static void decompressGzipFile(String gzipFile, String newFile) {
+		FileInputStream fis = null;
+		GZIPInputStream gis = null;
+		FileOutputStream fos = null;
 		try {
 
-			FileInputStream fis = new FileInputStream(gzipFile);
-			GZIPInputStream gis = new GZIPInputStream(fis);
-			FileOutputStream fos = new FileOutputStream(newFile);
+			fis = new FileInputStream(gzipFile);
+			gis = new GZIPInputStream(fis);
+			fos = new FileOutputStream(newFile);
 			byte[] buffer = new byte[1024];
 			int len;
 			while ((len = gis.read(buffer)) != -1) {
 				fos.write(buffer, 0, len);
 			}
-			// closing resources
-			fos.close();
-			gis.close();
+
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			// closing resources
+			try {
+				fos.close();
+				gis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 		}
 
 	}
@@ -47,23 +57,35 @@ public class ConvertToZip {
 	 * here
 	 */
 	private static void compressGzipFile(String file, String gzipFile) {
+		GZIPOutputStream gzipOS = null;
+		FileOutputStream fos = null;
+		FileInputStream fis = null;
 		try {
-			FileInputStream fis = new FileInputStream(file);
-			FileOutputStream fos = new FileOutputStream(gzipFile);
-			GZIPOutputStream gzipOS = new GZIPOutputStream(fos);
+			fis = new FileInputStream(file);
+			fos = new FileOutputStream(gzipFile);
+			gzipOS = new GZIPOutputStream(fos);
 			byte[] buffer = new byte[1024];
 			int len;
 			while ((len = fis.read(buffer)) > 0) {
 				gzipOS.write(buffer, 0, len);
 			}
-			// close resources
-			gzipOS.close();
-			fos.close();
-			fis.close();
+
 		} catch (IOException e) {
 			System.out.println(e);
+		} finally {
+			// close resources
+			try {
+				gzipOS.close();
+				fos.close();
+				fis.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
 		}
 
 	}
 
 }
+
+
