544597b995aab737bff9deca9493355ee27c5792
Subhragoswami <109273635+Subhragoswami@users.noreply.github.com>
2022-08-11 17:37:04 +0530

    Update FileCreation.java

--- filehandling/FileCreation.java
+++ filehandling/FileCreation.java
@@ -9,31 +9,32 @@ import java.util.Scanner;
 
 public class FileCreation {
 	public static void main(String[] args) {
-		File myFile = new File("FileHandlingExmple.txt");
+		File myFile = new File("FileHandling\\FileHandlingExmple.txt");
+		FileWriter fileWriter = null;
+		Scanner scanner = null;
+		BufferedWriter out = null;
 		try {
 			// Creating a file
 			myFile.createNewFile();
 			System.out.println("File created successfully.");
 			// Writing to a file
-			FileWriter fileWriter = new FileWriter("FileHandlingExmple.txt");
+			fileWriter = new FileWriter("FileHandling\\FileHandlingExmple.txt");
 			fileWriter.write("In this tutorial we will see how to create a file in Java using createNewFile() method.");
-			fileWriter.close();
+
 			// Reading a file
-			Scanner scanner = new Scanner(myFile);
+			scanner = new Scanner(myFile);
 			while (scanner.hasNextLine()) {
 				String line = scanner.nextLine();
 				System.out.println(line);
 			}
-			scanner.close();
-			String fileName = "FileHandlingExmple.txt";
+
+			String fileName = "FileHandling\\FileHandlingExmple.txt";
 			String text = "Hello world";
-			BufferedWriter out = new BufferedWriter(new FileWriter(fileName, true));
+			out = new BufferedWriter(new FileWriter(fileName, true));
 
 			// Writing on output stream
 			out.write(text);
-			// Closing the connection
-			out.close();
-			
+
 			// deleting a file-----
 			/*
 			 * if (myFile.delete()) { System.out.println("I have deleted: " +
@@ -41,11 +42,20 @@ public class FileCreation {
 			 * System.out.println("Some problem occurred while deleting the file"); }
 			 */
 			// Specifying the file name and path
-			File file = new File("FileHandlingExmple.txt");
+			File file = new File("FileHandling\\FileHandlingExmple.txt");
 			SimpleDateFormat lmf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 			System.out.println("Last modified date :" + lmf.format(file.lastModified()));
 		} catch (IOException e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				fileWriter.close();
+				out.close();
+			} catch (IOException e) {
+
+				e.printStackTrace();
+			}
+			scanner.close();
 		}
 	}
 }
